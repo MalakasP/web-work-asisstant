41b2c1b38b33d743379a607d4ee11f9ab8fd5088
MalakasP <48155363+MalakasP@users.noreply.github.com>
2021-05-01 14:41:27 +0300

    Mixed content error fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,6 @@
 
     <!-- Styles -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
 <body>
     <div id="app">
